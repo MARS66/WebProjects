d5cd468740fdbbc79502e4262de862528b45260d
MARS66 <1908367780@qq.com>
2021-12-23 17:54:07 +0800

    提交代码

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@
  * @Author: yukaiwei
  * @Date: 2021-07-07 09:50:04
  * @LastEditors: yukaiwei
- * @LastEditTime: 2021-12-20 15:25:48
+ * @LastEditTime: 2021-12-22 13:31:29
  */
 $(function (params) {
   var wTop=$(window).height();
@@ -19,12 +19,14 @@ $(function (params) {
     }else{
       $(this).addClass('menu_active')
     }
-    $('.nav .nav_items').animate({
+    $('.nav').animate({
       width:'toggle',
     },300)
   })
-
-
+  //  二级菜单显示隐藏
+  $('.nav .drop').click(function(){
+    $(this).siblings('.second_menu').slideToggle(300).parent('li').siblings('li').children('.second_menu').slideUp(300)
+  })
   // 鼠标滚动动画
   $(window).scroll(()=>{
     animation();
